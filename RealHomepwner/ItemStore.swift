67a20662a0168b1be58cc343482f1e2c6a20e31e
Sighmox <Sighmox@github.com>
2019-04-02 11:20:29 -0500

    Complete Chapter 16 with Bronze challenge

--- RealHomepwner/ItemStore.swift
+++ RealHomepwner/ItemStore.swift
@@ -12,12 +12,31 @@ class ItemStore {
     
     var allItems = [Item]()
     
+    init() {
+        if let archivedItems =
+            NSKeyedUnarchiver.unarchiveObject(withFile: itemArchiveURL.path) as? [Item] {
+            allItems = archivedItems
+        }
+    }
+    
+    let itemArchiveURL: URL = {
+        let documentsDirectories =
+            FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = documentsDirectories.first!
+        return documentDirectory.appendingPathComponent("items.archive")
+        }()
+    
     @discardableResult func createItem() -> Item {
         let newItem = Item(random: true)
         
         allItems.append(newItem)
         
         return newItem
+    }
+    
+    func saveChanges() -> Bool {
+        print("Saving items to: \(itemArchiveURL.path)")
+        return NSKeyedArchiver.archiveRootObject(allItems, toFile: itemArchiveURL.path)
     }
     
     func removeItem(_ item: Item) {
